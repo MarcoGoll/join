d8ec81fa86c890e10f1a2093bb1789c64c59bb2a
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-02-11 15:43:45 +0100

    create: contacts service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"join-82c5c","appId":"1:27899760680:web:a69a8cecc0f0971858a51d","storageBucket":"join-82c5c.firebasestorage.app","apiKey":"AIzaSyBmPjsLf9R76U3csMNtLgAhffJOZeh9Rvc","authDomain":"join-82c5c.firebaseapp.com","messagingSenderId":"27899760680"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
