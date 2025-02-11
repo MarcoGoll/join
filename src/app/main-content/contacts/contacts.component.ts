f9b6945c6e59e538e88b448a448535809cb7f8ea
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-02-11 14:11:40 +0100

    create: basic components structure

--- src/app/main-content/contacts/contacts.component.ts
+++ src/app/main-content/contacts/contacts.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { ContactslistComponent } from './contactslist/contactslist.component';
+import { ContactsdetailsComponent } from './contactsdetails/contactsdetails.component';
+import { HeaderComponent } from "../../shared/components/header/header.component";
+import { NavbarComponent } from "../../shared/components/navbar/navbar.component";
 
 @Component({
   selector: 'app-contacts',
   standalone: true,
-  imports: [],
+  imports: [ContactslistComponent, ContactsdetailsComponent, HeaderComponent, NavbarComponent],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss'
 })
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../shared/components/header/header.component';
+import { NavbarComponent } from '../shared/components/navbar/navbar.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, NavbarComponent, ContactsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
