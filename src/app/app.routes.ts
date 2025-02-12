844eb3b16d29a4c3a66460fc8ef840396f5cb69f
Ismail Baris <ismail.93.baris@gmail.com>
2025-02-12 02:02:46 +0100

    navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
+import { LegalNoticeComponent } from './shared/components/navbar/legal-notice/legal-notice.component';
+import { PrivacyPolicyComponent } from './shared/components/navbar/privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
     { path: '', component: MainContentComponent },
+    { path: 'legalNotice', component: LegalNoticeComponent },
+    { path: 'privacyPolicy', component: PrivacyPolicyComponent },
 ]; 
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, LegalNoticeComponent,PrivacyPolicyComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
