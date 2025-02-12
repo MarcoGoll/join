5df132da20d4c46032ff93651285015fadbf8324
Marzi Ghasedian <marzighased@gmail.com>
2025-02-12 18:51:06 +0100

    colors and details connections added

--- src/app/main-content/contacts/contactsdetails/contactsdetails.component.ts
+++ src/app/main-content/contacts/contactsdetails/contactsdetails.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ContactsService } from '../../../shared/services/firebase/contacts.service';
+import { Contact } from '../../../shared/interfaces/contact';
 
 @Component({
   selector: 'app-contactsdetails',
@@ -9,5 +11,30 @@ import { Component } from '@angular/core';
   styleUrl: './contactsdetails.component.scss'
 })
 export class ContactsdetailsComponent {
+
+
   selectedContent: any = true;
+  
+
+  constructor(public contactService: ContactsService) {
+
+    }
+
+    useEmail(){
+      this.contactService.currentlySelectedContact.email
+    }
+
+    useFirstName(){
+      this.contactService.currentlySelectedContact.firstName
+    }
+
+    useLastName(){
+      this.contactService.currentlySelectedContact.lastName
+    }
+
+    usePhoneNumber(){
+      this.contactService.currentlySelectedContact.phone
+    }
 }
+
+
