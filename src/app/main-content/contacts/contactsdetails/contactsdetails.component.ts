dc8da34d76406da2cf0d0ade838d2b7914a5c9e6
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-02-16 16:36:38 +0100

    create: logic for mobile fadein/out contact detailes

--- src/app/main-content/contacts/contactsdetails/contactsdetails.component.ts
+++ src/app/main-content/contacts/contactsdetails/contactsdetails.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, Input, isDevMode } from '@angular/core';
 import { ContactsService } from '../../../shared/services/firebase/contacts.service';
 import { Contact } from '../../../shared/interfaces/contact';
+import { ControlContainer } from '@angular/forms';
 
 @Component({
   selector: 'app-contactsdetails',
@@ -13,14 +14,22 @@ import { Contact } from '../../../shared/interfaces/contact';
 export class ContactsdetailsComponent {
   @Input("isMobileView") isMobileView: boolean = false;
   isMoreControlOverviewShown: boolean = false
+  isInitialLoad: boolean = true;
 
   constructor(public contactService: ContactsService) {
   }
 
   toggleMoreControlOverview() {
     this.isMoreControlOverviewShown = !this.isMoreControlOverviewShown;
+    this.isInitialLoad = false;
   }
 
+  setInitialValues() {
+    this.isInitialLoad = true;
+    this.isMoreControlOverviewShown = false;
+    console.log("isInitialLoad was set to: ", this.isInitialLoad);
+    console.log("isMoreControlOverviewShown has value: ", this.isMoreControlOverviewShown);
+  }
 }
 
 
