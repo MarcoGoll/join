a3ad8adbbcf5f9e46c8b979086fb5fe30e6ba3c6
Ismail Baris <ismail.93.baris@gmail.com>
2025-02-17 13:47:46 +0100

    change: privacy policy/ legal notice /info

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { MainContentComponent } from './main-content/main-content.component';
 import { LegalNoticeComponent } from './shared/components/navbar/legal-notice/legal-notice.component';
 import { PrivacyPolicyComponent } from './shared/components/navbar/privacy-policy/privacy-policy.component';
 import { ContactsComponent } from './main-content/contacts/contacts.component';
+import { InfoComponent } from './shared/components/header/info/info.component';
 
 export const routes: Routes = [
     { path: '', component: MainContentComponent },
     { path: 'legalNotice', component: LegalNoticeComponent },
     { path: 'privacyPolicy', component: PrivacyPolicyComponent },
-    { path: 'contact', component: ContactsComponent }
+    { path: 'contact', component: ContactsComponent },
+    { path: 'info', component: InfoComponent }
 ];  
