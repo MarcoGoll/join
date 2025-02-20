4325e642c3ced77efc2f8c36aa79e005ae0e4090
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-02-20 15:44:24 +0100

    create: draft add tasks

--- src/app/main-content/add-task/add-task.component.ts
+++ src/app/main-content/add-task/add-task.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { TasksService } from '../../shared/services/firebase/tasks.service';
+import { Task } from '../../shared/interfaces/task';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-task.component.html',
   styleUrls: ['./add-task.component.scss', './add-task.responsive.scss']
 })
@@ -12,4 +14,14 @@ export class AddTaskComponent {
 
   taskService = inject(TasksService);
 
+  newTask: Task = {
+    "title": "",
+    "description": "",
+    "assignedTo": [],
+    "status": "toDo",
+    "dueDate": "yyyy-mm-dd",
+    "prio": "Medium",
+    "category": "User Story",
+    "subTasks": [],
+  }
 }
