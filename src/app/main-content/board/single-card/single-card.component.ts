57dd5a8f12b351826d5b1ce2e0b3f073b0485c7f
Ismail Baris <ismail.93.baris@gmail.com>
2025-02-24 06:17:24 +0100

    changed: Single-card component html,scss,service implementation

--- src/app/main-content/board/single-card/single-card.component.ts
+++ src/app/main-content/board/single-card/single-card.component.ts
@@ -1,14 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { Task } from '../../../shared/interfaces/task';
+import { CommonModule } from '@angular/common';
+import { TasksService } from '../../../shared/services/firebase/tasks.service';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-single-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './single-card.component.html',
-  styleUrl: './single-card.component.scss'
+  styleUrls: ['./single-card.component.scss', './single-card.responsive.scss']
 })
 export class SingleCardComponent {
+
+  taskService = inject(TasksService);
+
   @Input("task") task: Task = {
     "title": "",
     "description": "",
